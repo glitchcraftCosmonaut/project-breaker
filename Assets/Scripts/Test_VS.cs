63a1e1cb03ddcde0252451d0c73c0327ebe43c28
Rhaiz <55672309+glitchcraftCosmonaut@users.noreply.github.com>
2024-02-07 15:17:43 +0700

    INIT_COMMIT
    test vscodium dependencies
    Result: Success

---
+++ Assets/Scripts/Test_VS.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_VS : MonoBehaviour
+{
+    string str = "Test";
+    // Start is called before the first frame update
+    void Start()
+    {
+        // GameObject gameObject = new GameObject("Test");
+        Debug.Log(str);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
